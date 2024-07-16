25cd50218b3152741dcf4d28925b8f8eb203c845
Guilherme Mene <guilherme.ale.primo@live.com>
2024-07-16 23:54:46 +0100

    Finish the recipes api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,5 +12,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fiels = {'id', 'title', 'time_minutes', 'prioce', 'link'}
-        read_only_fields = {'id'}
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
